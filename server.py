f104f7227e24124238f61efbf7db1bc441717b1b
Carson Mulligan <103183546+carsonmulligan@users.noreply.github.com>
2025-01-29 16:58:29 -0500

    ♻️ refactor: Update ChapterLoader to use test content and modify loadChapter method for better error handling

--- server.py
+++ server.py
@@ -1,1 +1,9 @@
- 
+from http.server import SimpleHTTPRequestHandler
+from socketserver import TCPServer
+
+class CORSRequestHandler(SimpleHTTPRequestHandler):
+    def end_headers(self):
+        self.send_header('Access-Control-Allow-Origin', '*')
+        super().end_headers()
+
+TCPServer(('', 8000), CORSRequestHandler).serve_forever() 
